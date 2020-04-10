4ffcba760121fded4c991b9e3657ab955d37d0f8
Isabela Silva <eu.silvaisabela@gmail.com>
2020-04-09 21:06:11 -0300

    Add list file App

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
+
+if (module.hot){
+  module.hot.accept();
+}
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
